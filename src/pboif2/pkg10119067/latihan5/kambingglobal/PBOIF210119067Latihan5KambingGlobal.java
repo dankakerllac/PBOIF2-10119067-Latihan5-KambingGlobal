8f276a560774641063d336fc748c6d7aef87c8c7
dankakerllac <72844005+dankakerllac@users.noreply.github.com>
2020-10-19 22:43:08 +0700

    Update PBOIF210119067Latihan5KambingGlobal.java

--- src/pboif2/pkg10119067/latihan5/kambingglobal/PBOIF210119067Latihan5KambingGlobal.java
+++ src/pboif2/pkg10119067/latihan5/kambingglobal/PBOIF210119067Latihan5KambingGlobal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pboif2.pkg10119067.latihan2.deklarasivariabel;
+package pboif2.pkg10119067.latihan2;
 
 /**
  *
